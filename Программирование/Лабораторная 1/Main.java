1ed971847bd2c5581be3dd99458d998f17ad0d6d
potterandrey4 <potterandrey4@gmail.com>
2022-09-30 15:41:20 +0300

    Update Main.java

--- Программирование/Лабораторная 1/Main.java
+++ Программирование/Лабораторная 1/Main.java
@@ -19,18 +19,23 @@ public class Main {
         double[][] p = new double[17][14];
         for (int i = 0; i< a.length; i++) {
             for (int j = 0; j<x.length; j++) {
-                if (a[i] == 2) {
+                switch (a[i] == 2) {
 
-                    p[i][j] = Math.cos( Math.cbrt( Math.pow( (0.5/(1-x[j])), 3 ) ) );
+                    case (2) {
+                        p[i][j] = Math.cos( Math.cbrt( Math.pow( (0.5/(1-x[j])), 3 ) ) );
+                        break;
+                    }
 
-                } else if (a[i] == 5 | a[i] == 6 | a[i] == 7 | a[i] == 8 | a[i] == 10 | a[i] == 12 | a[i] == 13 | a[i] == 14) {
-                    p[i][j] = Math.pow( (Math.log( Math.pow( (Math.abs(x[j])/5), x[j] ) - 1 ) ), ( (Math.pow(3, -1)) / (0.25 + Math.pow( (3 * Math.pow(x[j], -1)), 3 ))) );
+                    case (5 | 6 | 7 | 8 |10 | 12 | 13 | 14) {
+                        p[i][j] = Math.pow( (Math.log( Math.pow( (Math.abs(x[j])/5), x[j] ) - 1 ) ), ( (Math.pow(3, -1)) / (0.25 + Math.pow( (3 * Math.pow(x[j], -1)), 3 ))) );
+                        break;
+                    } 
 
-                } else {
+                    default {
 
-                    p[i][j] = Math.cos( Math.pow( (Math.PI / (Math.pow(Math.atan((x[j]+1)/2), 2) + 0.5) ), Math.tan(Math.cos(x[j]) ) ) );
-
-                }
+                        p[i][j] = Math.cos( Math.pow( (Math.PI / (Math.pow(Math.atan((x[j]+1)/2), 2) + 0.5) ), Math.tan(Math.cos(x[j]) ) ) );
+                        
+                    }
 
             }
         }
@@ -38,7 +43,7 @@ public class Main {
         for (int i=0; i<p.length; i++) {
             System.out.print("[ ");
             for (int j=0; j<p[i].length; j++) {
-                System.out.printf("%.3f; ", p[i][j]);
+                System.out.printf("%.3f ", p[i][j]);
             }
             System.out.println("] ");
         }
